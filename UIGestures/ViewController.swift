4109079f7cbca0ecce59eab11db20ba2fe60da82
Anastasia Legert <legertlab@gmail.com>
2020-02-26 13:59:58 +0300

    Initial Commit

---
+++ UIGestures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIGestures
+//
+//  Created by Anastasia Legert on 26/2/20.
+//  Copyright © 2020 Anastasia Legert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
